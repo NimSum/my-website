e439b15851540dfc47eff9c76c30f6e758d32584
Nimrod Garcia <nimm.garcia@gmail.com>
2019-04-24 17:21:32 -0600

    Add hover state to show/hide info

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,38 +4,50 @@ export class Project extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      showInfo: false
     }
   }
+
+  showInfo = () => {
+    this.setState( {showInfo: !this.state.showInfo} );
+  }
+
   render() {
     return (
-      <article className="project-box">
+      <article 
+        className="project-box"
+        onMouseEnter={this.showInfo}
+        onMouseLeave={this.showInfo}>
         <img 
           src={require(`../images/git-commander.jpg`)} 
           alt={this.props.title}
           className="portfolio-img" />
-        <section className="info-container">
-          <h3>{ this.props.title }</h3>
-          <h4>{ this.props.description }</h4>
-          <div className="info-box">
-            { Array.isArray(this.props.team) 
-              ? <p className="team-box">
-                  <span span className="title">Team</span>
-                  {this.props.team.map((person, idx) => 
-                    <span key={ idx }>
-                      <a href={ Object.values(person) }>
-                        { Object.keys(person)[0].split(' ')[0] }
-                      <img 
-          src={require(`../images/github.svg`)} alt="Github"/>
-                      </a>
-                  </span>)}
-                </p>
-              : <p className="solo-project">{ this.props.team }</p> 
-            }
-            <p><span className="title">Tools</span>{ this.props.toolsUsed.map(tool => <span>{ tool }</span>)}</p>
-            <p><span className="title">Concepts</span>{ this.props.concepts.map(concept => <span>{ concept }</span>)}</p>
-          </div>
-        </section>
+        { this.state.showInfo &&
+          (<section className="info-container">
+            <h3>{ this.props.title }</h3>
+            <h4>{ this.props.description }</h4>
+            <div className="info-box">
+              { Array.isArray(this.props.team) 
+                ? <p className="team-box">
+                    <span span className="title">Team</span>
+                    {this.props.team.map((person, idx) => 
+                      <span key={ idx }>
+                        <a href={ Object.values(person) }
+                          target="_blank" 
+                          rel="noopener noreferrer"  >
+                          { Object.keys(person)[0].split(' ')[0] }
+                        <img 
+            src={require(`../images/github.svg`)} alt="Github"/>
+                        </a>
+                    </span>)}
+                  </p>
+                : <p className="solo-project">{ this.props.team }</p> 
+              }
+              <p><span className="title">Tools</span>{ this.props.toolsUsed.map(tool => <span>{ tool }</span>)}</p>
+              <p><span className="title">Concepts</span>{ this.props.concepts.map(concept => <span>{ concept }</span>)}</p>
+            </div>
+          </section>)
+        }
         <div className="icon-container">
             <a 
               href={ this.props.staticSite } 
