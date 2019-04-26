c5e50144c6e664e42b138e9989102e9656092ebf
Nimrod Garcia <nimm.garcia@gmail.com>
2019-04-25 18:28:24 -0600

    Updates

---
+++ precache-manifest.3e65531facf7fda14877d72fd95a4cca.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "2367583f725cdf23ec19",
+    "url": "/my-website/static/css/main.f304dc6d.chunk.css"
+  },
+  {
+    "revision": "2367583f725cdf23ec19",
+    "url": "/my-website/static/js/main.f81aa440.chunk.js"
+  },
+  {
+    "revision": "37063c54184f9fd8244e",
+    "url": "/my-website/static/js/runtime~main.99d9029d.js"
+  },
+  {
+    "revision": "db1553b7be0c0b77ce66",
+    "url": "/my-website/static/js/2.0a751726.chunk.js"
+  },
+  {
+    "revision": "40dfc54e1753e3263a6aa3de9d92fe43",
+    "url": "/my-website/static/media/github.40dfc54e.svg"
+  },
+  {
+    "revision": "c6dde0cda1717da1ec9483866ec87f82",
+    "url": "/my-website/static/media/linked-in.c6dde0cd.svg"
+  },
+  {
+    "revision": "03ee2dac2b107be45274a3efb7af381c",
+    "url": "/my-website/static/media/github-blue.03ee2dac.svg"
+  },
+  {
+    "revision": "9bef1f34f8d748afdc97030754c925af",
+    "url": "/my-website/static/media/nimsum.9bef1f34.jpg"
+  },
+  {
+    "revision": "a5ca14dabf0acfcad29ee8587b01850e",
+    "url": "/my-website/static/media/mail.a5ca14da.svg"
+  },
+  {
+    "revision": "d887870045b080ffc114ac72464d19ae",
+    "url": "/my-website/static/media/react.d8878700.svg"
+  },
+  {
+    "revision": "a5ab8a67d52562b26d6c9dbef5b7d423",
+    "url": "/my-website/static/media/sass.a5ab8a67.svg"
+  },
+  {
+    "revision": "d3fdd45568517f351f2cf2084e627614",
+    "url": "/my-website/static/media/source.d3fdd455.svg"
+  },
+  {
+    "revision": "97047ac556faf2cdb8a83213901eed0f",
+    "url": "/my-website/static/media/bg-cont-1.97047ac5.svg"
+  },
+  {
+    "revision": "f60e24d61811d1f482fcd56b76f1b148",
+    "url": "/my-website/static/media/bg-cont-2.f60e24d6.svg"
+  },
+  {
+    "revision": "0d1b843834d5e8bab0747ad694470704",
+    "url": "/my-website/static/media/splash.0d1b8438.jpg"
+  },
+  {
+    "revision": "aee923463341ebf95c512dd2282a1bdd",
+    "url": "/my-website/static/media/connect-four.aee92346.jpg"
+  },
+  {
+    "revision": "371c920ba10a46409479a49bd9cfc571",
+    "url": "/my-website/static/media/css.371c920b.svg"
+  },
+  {
+    "revision": "3d3e21fe038b60a21d6230fc6687c8cf",
+    "url": "/my-website/static/media/foto-finder.3d3e21fe.jpg"
+  },
+  {
+    "revision": "b55ad4c615146a3d6de0a9e44e395ada",
+    "url": "/my-website/static/media/git-commander.b55ad4c6.jpg"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/my-website/static/media/github-black.76d344f0.svg"
+  },
+  {
+    "revision": "ffa30b207f0ee5b0d869183e3044df5c",
+    "url": "/my-website/static/media/html-5.ffa30b20.svg"
+  },
+  {
+    "revision": "ca5f8861fbe62f421378820a060b75dd",
+    "url": "/my-website/static/media/js.ca5f8861.svg"
+  },
+  {
+    "revision": "fb4ed52a3e30e35a18d4baa0ed51806c",
+    "url": "/my-website/static/media/marvelously.fb4ed52a.jpg"
+  },
+  {
+    "revision": "87f67679c45a8c71893263b8ecb5fff8",
+    "url": "/my-website/static/media/node-js.87f67679.svg"
+  },
+  {
+    "revision": "63106c0a67870233bbf8e01caa98bd99",
+    "url": "/my-website/static/media/static-comp.63106c0a.jpg"
+  },
+  {
+    "revision": "74b34f8c05186d988150caa65bca1814",
+    "url": "/my-website/static/media/usaf-photo.74b34f8c.jpg"
+  },
+  {
+    "revision": "56e8a83f116d8f4ed067b33a36a6c9cc",
+    "url": "/my-website/static/media/wheel-of-mortys.56e8a83f.jpg"
+  },
+  {
+    "revision": "f1390e8d6801a04a7fef625477e06717",
+    "url": "/my-website/index.html"
+  }
+];
