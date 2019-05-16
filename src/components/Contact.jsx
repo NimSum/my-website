b770b1ecf4ac1d5ee418257112bc4e365a0c21b4
Nimrod Garcia <nimm.garcia@gmail.com>
2019-05-15 19:30:10 -0600

    Change skills box font sizing, colors, and layout

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -81,7 +81,7 @@ export class Contact extends Component {
         <article>
           <div className="contact-intro">
             <h3>Let's get in touch!</h3>
-            <p>I am currently searching for an entry level Front End Developer position. If you are interested, please send me a message! I will get back with you as soon as I can! </p>
+            <p>I am searching for an entry level Front End Developer position in Dallas. If you are interested, please send me a message! I will get back with you as soon as I can! </p>
           </div>
           <div className="icon-container">
             <a href="https://github.com/NimSum" target="_blank" rel="noopener noreferrer" title="Github">
