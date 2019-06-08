3592ef4b532e241e144b6152f8d238b3c1769fb5
Nimrod Garcia <nimm.garcia@gmail.com>
2019-06-08 16:46:22 -0600

    Converted to functional component

--- src/components/Splash.jsx
+++ src/components/Splash.jsx
@@ -1,37 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export class Splash extends Component {
-  stuff() {
-    console.log('hi')
-  }
-  render() {
-    return (
-      <section className="splash" id="home">
-        <article>
-          <h1>Hi</h1>
-          <div className="splash-intro">
-            <p>I'm <span>Nimrod Garcia,</span> </p>  
-            <p>a <span>Front-end Software Engineer</span></p>
-          </div>
-          <div className="splash-icon-container">
-            <a href="https://github.com/NimSum" target="_blank" rel="noopener noreferrer">
-              <img src={require('../images/github-blue.svg')} alt="github" />
-            </a>
-            <a href="https://www.linkedin.com/in/nimrod-garcia/" target="_blank" rel="noopener noreferrer">
-              <img src={require('../images/linked-in.svg')} alt="linked in" />
-            </a>
-          </div>
-        </article>
-        <div className="arrow-down-container">
-          <div className="arrow-down" onClick={this.stuff}>
-            <a href="#about">About me</a>
-          </div>
-          <span className="arrow-down-desc">ABOUT ME</span>
+const Splash = () => (
+    <section className="splash" id="home">
+      <article>
+        <h1>Hi</h1>
+        <div className="splash-intro">
+          <p>I'm <span>Nimrod Garcia,</span> </p>  
+          <p>a <span>Front-end Software Engineer</span></p>
         </div>
-        <span className="photo-credit">Photo by Joel Filipe on Unsplash</span>
-      </section>
-    )
-  }
-}
+        <div className="splash-icon-container">
+          <a href="https://github.com/NimSum" target="_blank" rel="noopener noreferrer">
+            <img src={require('../images/github-blue.svg')} alt="github" />
+          </a>
+          <a href="https://www.linkedin.com/in/nimrod-garcia/" target="_blank" rel="noopener noreferrer">
+            <img src={require('../images/linked-in.svg')} alt="linked in" />
+          </a>
+        </div>
+      </article>
+      <div className="arrow-down-container">
+        <div className="arrow-down">
+          <a href="#about">About me</a>
+        </div>
+        <span className="arrow-down-desc">ABOUT ME</span>
+      </div>
+      <span className="photo-credit">Photo by Joel Filipe on Unsplash</span>
+    </section>
+)
 
-export default Splash
+export default Splash;
