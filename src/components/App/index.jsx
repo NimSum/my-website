02b9914f61e2c8b8af3ca880fdd2cb80cecbd7b4
Nimrod Garcia <nimm.garcia@gmail.com>
2019-06-08 17:03:55 -0600

    Transfered all files to their own containing folder for better readability

---
+++ src/components/App/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './index.scss';
+import Splash from '../Splash';
+import MainSection from '../MainSection';
+
+const App = () => {
+  return (
+    <div className="App">
+      < Splash />
+      < MainSection />
+    </div>
+  );
+}
+
+export default App;
--- src/components/MainSection/index.jsx
+++ src/components/MainSection/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import Navbar from './Navbar';
+import Navbar from '../Navbar';
 import About from './About';
-import Contact from './Contact';
-import Portfolio from './Portfolio';
+import Contact from '../Contact';
+import Portfolio from '../Portfolio';
 
 const MainSection = () => {
   return (
