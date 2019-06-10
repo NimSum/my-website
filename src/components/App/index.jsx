ea4a188007bda5f2c86395d3485c61824fe28722
Nimrod Garcia <nimm.garcia@gmail.com>
2019-06-10 15:17:32 -0600

    Add animation for onload

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,23 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import Splash from '../Splash';
 import MainSection from '../MainSection';
+import loadingIcon from '../../images/loading.svg';
 
-const App = () => {
-
-  const [ isLoading, setLoading ] = useState(false);
-
-  const updateLoading = (bool) => {
-    setLoading(bool)
-  }
-
-
-
-  return isLoading
-    ? <div>HIII</div>
-    : <div className="App">
-        < Splash setLoading={ updateLoading } />
-        < MainSection />
-      </div>
-}
+const App = () => (
+  <div className="App">
+    < Splash />
+    < MainSection />
+  </div>
+)
 
 export default App;
--- src/components/Splash/index.jsx
+++ src/components/Splash/index.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
-const Splash = ({ setLoading }) => (
+const Splash = () => (
     <section className="splash" id="home">
-      <img className='bg-image' src={require("../../images/splash.jpg")} alt="sd"/>
+      <img 
+        className='bg-image' 
+        src={require("../../images/splash.jpg")} 
+        alt="sd"/>
       <article>
         <h1>Hi</h1>
         <div className="splash-intro">
