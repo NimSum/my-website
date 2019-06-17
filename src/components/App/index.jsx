7034a330b0161c72eb6d4f9322b7e9e9d4d5f2f0
Nimrod Garcia <nimm.garcia@gmail.com>
2019-06-16 20:11:15 -0600

    Convert App component to class component

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,13 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import Splash from '../Splash';
 import MainSection from '../MainSection';
 import loadingIcon from '../../images/loading.svg';
 
-const App = () => (
-  <div className="App">
-    < Splash />
-    < MainSection />
-  </div>
-)
+export class App extends Component {
+
+
+  render() {
+    return (
+    <div className="App">
+      < Splash />
+      < MainSection />
+    </div>
+    )
+  }
+} 
 
 export default App;
