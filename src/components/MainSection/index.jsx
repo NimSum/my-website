e2dce9d9c496fe9c97450df3b4f8e7968e921875
Nimrod Garcia <nimm.garcia@gmail.com>
2019-11-22 14:34:41 -0600

    Create current section state

--- src/components/MainSection/index.jsx
+++ src/components/MainSection/index.jsx
@@ -1,18 +1,27 @@
-import React from 'react'
-import Navbar from '../Navbar';
-import About from '../About';
-import Contact from '../Contact';
-import Portfolio from '../Portfolio';
+import React, { useState } from "react";
+import Navbar from "../Navbar";
+import About from "../About";
+import Contact from "../Contact";
+import Portfolio from "../Portfolio";
 
 const MainSection = () => {
-  return (
-    <main className="main-section">
-      < Navbar />
-      < About />
-      < Portfolio />
-      < Contact />
-    </main>
-  )
-}
+	const [currentSection, setCurrentSection] = useState("Home");
 
-export default MainSection
+	const handleSetCurrentSection = section => {
+		setCurrentSection(section);
+	};
+
+	return (
+		<main className="main-section">
+			<Navbar
+				currentSection={currentSection}
+				handleSetCurrentSection={handleSetCurrentSection}
+			/>
+			<About />
+			<Portfolio currentSection={currentSection} />
+			<Contact />
+		</main>
+	);
+};
+
+export default MainSection;
