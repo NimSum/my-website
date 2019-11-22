ce74ac71783afd94c44fa776b77d7ea24d10ba89
Nimrod Garcia <nimm.garcia@gmail.com>
2019-11-22 14:39:00 -0600

    Removes java as skill

--- src/utils/PortfolioData.js
+++ src/utils/PortfolioData.js
@@ -1,234 +1,301 @@
 export const projects = [
-  {
-    "title": "Palette Pal",
-    "description": "A full stack app that allows users to create color palettes for their projects. Users can also sign up to save, modify or add additional color palettes.",
-    "team" : [ 
-      { "Lynne Rang(Frontend)" : "https://github.com/lynnerang" }
-    ],
-    "toolsUsed": ["React", "Node.js & Express", "Jest & Enzyme", "JWT Authentication", "Travis CI"],
-    "concepts": ["Express Middleware", "Pair Programming", "UI/UX Design", "TDD"],
-    "src": "https://github.com/NimSum/palette-pal-frontend",
-    "staticSite": " https://nimsum.github.io/palette-pal-frontend/",
-    "imgURL": "palette-pal.jpg",
-    "id": "11"
-  },
-  {
-    "title": "CoderDojo Colorado",
-    "description": "An open source app that helps computer programming organizations to create/manage events and dojos in Denver, students can attend events to gain achievements by completing goals.",
-    "team" : [ 
-      { "Tiffany Bacher(Frontend)" : "https://github.com/tiffanybacher" },
-      { "Erik Konner(Frontend)" : "https://github.com/ehk9000" },
-      { "Matt Weiss(Backend)" : "https://github.com/Matt-Weiss" },
-    ],
-    "toolsUsed": ["React", "React Router", "Jest & Enzyme", "GraphQL(Apollo)", "Travis CI"],
-    "concepts": ["Agile Development", "GitHub Workflow", "Backend Collaboration", "UI/UX Design"],
-    "src": "https://github.com/turing-coderdojo",
-    "staticSite": "https://coderdojo-colorado.herokuapp.com",
-    "imgURL": "coderdojo-denver.jpg",
-    "id": "11"
-  },
-  {
-    "title": "Harvard Art Museum Explorer",
-    "description": "This app was shows images taken by Harvard Musuem of Arts and also allows for exploring the art within the museum. The requirements for this project were to learn and create a Vue app within 5 days",
-    "team" : "Solo Project",
-    "toolsUsed": ["Vue", "Vue Router", "Jest & vue-test-utils"],
-    "concepts": ["Rapid Development", "GitHub Workflow", "Async JS Testing", "UI/UX Design"],
-    "src": "https://github.com/NimSum/harvard-art-museum",
-    "staticSite": "https://harvard-museum-explorer.herokuapp.com/",
-    "imgURL": "harvard-museum.jpg",
-    "id": "10"
-  },
-  {
-    "title": "SpaceXplorer",
-    "description": "A multi-page app that allows users to find out when the next rocket launches are, they also can learn about rockets, astronauts and more!",
-    "team" : "Solo Project",
-    "toolsUsed": ["React", "Redux", "SASS", "Jest & Enzyme", "React-router"],
-    "concepts": ["User Stories", "GitHub Workflow", "Async JS Testing", "UI/UX Design"],
-    "src": "https://github.com/NimSum/spacexplorer",
-    "staticSite": "https://nimsum.github.io/spacexplorer/#/",
-    "imgURL": "space-x-plorer.jpg",
-    "id": "10"
-  },
-  {
-    "title": "Trapper Keeper",
-    "description": "A replication of the google app called Google Keep, users can create notes/check lists. Users can also delete/edit/change the color theme of each card",
-    "team" : [ 
-      { "Brennan Duffey" : "https://github.com/BrennanDuffey" },
-      { "Bridgett Coyle" : "https://github.com/B-Coyle" },
-      { "Adam Niedzwiecki" : "https://github.com/AdamN8142" } ],
-    "toolsUsed": ["Node & Express", "React", "Redux", "SASS", "Jest & Enzyme", "React-router"],
-    "concepts": ["GitHub Workflow", "Async JS Testing", "Back-end Development", "Back-end Testing", "Team Work"],
-    "src": "https://github.com/NimSum/trapper-keeper",
-    "staticSite": null,
-    "imgURL": "trapper-keeper.jpg",
-    "id": "9"
-  },
-  {
-    "title": "git Commander",
-    "description": "An interactive game that trains developers git commands ",
-    "team" : "Solo Project",
-    "toolsUsed": ["React", "SASS", "Jest & Enzyme"],
-    "concepts": ["User Stories", "GitHub Workflow", "TDD", "API Fetches", "UI/UX Design"],
-    "src": "https://github.com/NimSum/git-commander",
-    "staticSite": "https://nimsum.github.io/git-commander/",
-    "imgURL": "git-commander.jpg",
-    "id": "8"
-  },
-  {
-    "title": "Movie Tracker",
-    "description": "A page that shows the most recent movies showing in theaters. Features a login/signup functionality for users to save their favorites",
-    "team" : [ { "Kelly Zick" : "https://github.com/misskelly" } ],
-    "toolsUsed": ["React", "Redux", "React-router", "SASS", "Jest & Enzyme", "PropTypes"],
-    "concepts": ["Async JS", "Organized Workflow", "TDD", "Async JS Testing", 'Wireframing'],
-    "src": "https://github.com/NimSum/movie-tracker",
-    "staticSite": "https://nimsum.github.io/movie-tracker/",
-    "imgURL": "movie-tracker.jpg",
-    "id": "7"
-  },
-  {
-    "title": "Star Wars api box",
-    "description": "A page that uses the swapi api to generate cards about Star Wars characters/vehicles/planets",
-    "team" : "Solo Project",
-    "toolsUsed": ["React", "SASS", "Jest & Enzyme", "PropTypes"],
-    "concepts": ["Async JS", "Organized Workflow", "TDD", "Async JS Testing"],
-    "src": "https://github.com/NimSum/swapi-box",
-    "staticSite": "https://nimsum.github.io/swapi-box/",
-    "imgURL": "swapi-box.jpg",
-    "id": "6"
-  },
-  {
-    "title": "Marvelously",
-    "description": "A page that showcases comics and movies, it has a powerful search and sorting functionality",
-    "team" : [
-      { "Hannah Hyde" : "https://github.com/alizarincrimson" },
-      { "Max Silver" : "https://github.com/MaxBSilver" },
-      { "Raechel Odom" : "https://github.com/raechelo" }
-    ],
-    "toolsUsed": ["React", "SASS", "Jest & Enzyme"],
-    "concepts": ["Team Work", "TDD", "Git Workflow", "API Fetches"],
-    "src": "https://github.com/NimSum/marvel-whateverly",
-    "staticSite": "https://nimsum.github.io/marvel-whateverly/",
-    "imgURL": "marvelously.jpg",
-    "id": "5"
-  },
-  {
-    "title": "Wheel of Mortys",
-    "description": "Wheel of fortune Rick and Morty Style!",
-    "team" : [ { "Raechel Odom" : "https://github.com/raechelo" } ],
-    "toolsUsed": ["Webpack", "Mocha & Chai", "SASS"],
-    "concepts": ["OOP", "TDD", "Paired Programming", "Git Workflow"],
-    "src": "https://github.com/NimSum/wheel-of-mortys",
-    "staticSite": "https://nimsum.github.io/wheel-of-mortys/",
-    "imgURL": "wheel-of-mortys.jpg",
-    "id": "4"
-  },
-  {
-    "title": "Connect Four",
-    "description": "A replication of the game connect four!",
-    "team" : "Solo Project",
-    "toolsUsed": ["HTML", "CSS", "JS"],
-    "concepts": ["Vanilla JS", "Array Prototypes", "Game Structure Design"],
-    "src": "https://github.com/NimSum/connect-four-game",
-    "staticSite": "https://nimsum.github.io/connect-four-game/index.html",
-    "imgURL": "connect-four.jpg",
-    "id": "3"
-  },
-  {
-    "title": "Static Page Recreation",
-    "description": "Re-create a complex page layout, including my own styling and animations",
-    "team" : "Solo Project",
-    "toolsUsed": ["HTML", "CSS", "Chart.js"],
-    "concepts": ["BEM/SMACSS", "Grid Areas", "Animations"],
-    "src": "https://github.com/NimSum/ng-comp-challenge-2",
-    "staticSite": "https://nimsum.github.io/ng-comp-challenge-2/",
-    "imgURL": "static-comp.jpg",
-    "id": "2"
-  },
-  {
-    "title": "Foto Finder",
-    "description": "An app that saves images and text to local storage, with features such as: filtering, searching & favorites ",
-    "team" : "Solo Project",
-    "toolsUsed": ["CSS", "HTML", "JS"],
-    "concepts": ["es6", "Grid", "Local Storage"],
-    "src": "https://github.com/NimSum/foto-finder",
-    "staticSite": "https://nimsum.github.io/foto-finder/",
-    "imgURL": "foto-finder.jpg",
-    "id": "1"
-  }
-]
+	{
+		title: "Palette Pal",
+		description:
+			"A full stack app that allows users to create color palettes for their projects. Users can also sign up to save, modify or add additional color palettes.",
+		team: [{ "Lynne Rang(Frontend)": "https://github.com/lynnerang" }],
+		toolsUsed: [
+			"React",
+			"Node.js & Express",
+			"Jest & Enzyme",
+			"JWT Authentication",
+			"Travis CI"
+		],
+		concepts: ["Express Middleware", "Pair Programming", "UI/UX Design", "TDD"],
+		src: "https://github.com/NimSum/palette-pal-frontend",
+		staticSite: " https://nimsum.github.io/palette-pal-frontend/",
+		imgURL: "palette-pal.jpg",
+		id: "11"
+	},
+	{
+		title: "CoderDojo Colorado",
+		description:
+			"An open source app that helps computer programming organizations to create/manage events and dojos in Denver, students can attend events to gain achievements by completing goals.",
+		team: [
+			{ "Tiffany Bacher(Frontend)": "https://github.com/tiffanybacher" },
+			{ "Erik Konner(Frontend)": "https://github.com/ehk9000" },
+			{ "Matt Weiss(Backend)": "https://github.com/Matt-Weiss" }
+		],
+		toolsUsed: [
+			"React",
+			"React Router",
+			"Jest & Enzyme",
+			"GraphQL(Apollo)",
+			"Travis CI"
+		],
+		concepts: [
+			"Agile Development",
+			"GitHub Workflow",
+			"Backend Collaboration",
+			"UI/UX Design"
+		],
+		src: "https://github.com/turing-coderdojo",
+		staticSite: "https://coderdojo-colorado.herokuapp.com",
+		imgURL: "coderdojo-denver.jpg",
+		id: "11"
+	},
+	{
+		title: "Harvard Art Museum Explorer",
+		description:
+			"This app was shows images taken by Harvard Musuem of Arts and also allows for exploring the art within the museum. The requirements for this project were to learn and create a Vue app within 5 days",
+		team: "Solo Project",
+		toolsUsed: ["Vue", "Vue Router", "Jest & vue-test-utils"],
+		concepts: [
+			"Rapid Development",
+			"GitHub Workflow",
+			"Async JS Testing",
+			"UI/UX Design"
+		],
+		src: "https://github.com/NimSum/harvard-art-museum",
+		staticSite: "https://harvard-museum-explorer.herokuapp.com/",
+		imgURL: "harvard-museum.jpg",
+		id: "10"
+	},
+	{
+		title: "SpaceXplorer",
+		description:
+			"A multi-page app that allows users to find out when the next rocket launches are, they also can learn about rockets, astronauts and more!",
+		team: "Solo Project",
+		toolsUsed: ["React", "Redux", "SASS", "Jest & Enzyme", "React-router"],
+		concepts: [
+			"User Stories",
+			"GitHub Workflow",
+			"Async JS Testing",
+			"UI/UX Design"
+		],
+		src: "https://github.com/NimSum/spacexplorer",
+		staticSite: "https://nimsum.github.io/spacexplorer/#/",
+		imgURL: "space-x-plorer.jpg",
+		id: "10"
+	},
+	{
+		title: "Trapper Keeper",
+		description:
+			"A replication of the google app called Google Keep, users can create notes/check lists. Users can also delete/edit/change the color theme of each card",
+		team: [
+			{ "Brennan Duffey": "https://github.com/BrennanDuffey" },
+			{ "Bridgett Coyle": "https://github.com/B-Coyle" },
+			{ "Adam Niedzwiecki": "https://github.com/AdamN8142" }
+		],
+		toolsUsed: [
+			"Node & Express",
+			"React",
+			"Redux",
+			"SASS",
+			"Jest & Enzyme",
+			"React-router"
+		],
+		concepts: [
+			"GitHub Workflow",
+			"Async JS Testing",
+			"Back-end Development",
+			"Back-end Testing",
+			"Team Work"
+		],
+		src: "https://github.com/NimSum/trapper-keeper",
+		staticSite: null,
+		imgURL: "trapper-keeper.jpg",
+		id: "9"
+	},
+	{
+		title: "git Commander",
+		description: "An interactive game that trains developers git commands ",
+		team: "Solo Project",
+		toolsUsed: ["React", "SASS", "Jest & Enzyme"],
+		concepts: [
+			"User Stories",
+			"GitHub Workflow",
+			"TDD",
+			"API Fetches",
+			"UI/UX Design"
+		],
+		src: "https://github.com/NimSum/git-commander",
+		staticSite: "https://nimsum.github.io/git-commander/",
+		imgURL: "git-commander.jpg",
+		id: "8"
+	},
+	{
+		title: "Movie Tracker",
+		description:
+			"A page that shows the most recent movies showing in theaters. Features a login/signup functionality for users to save their favorites",
+		team: [{ "Kelly Zick": "https://github.com/misskelly" }],
+		toolsUsed: [
+			"React",
+			"Redux",
+			"React-router",
+			"SASS",
+			"Jest & Enzyme",
+			"PropTypes"
+		],
+		concepts: [
+			"Async JS",
+			"Organized Workflow",
+			"TDD",
+			"Async JS Testing",
+			"Wireframing"
+		],
+		src: "https://github.com/NimSum/movie-tracker",
+		staticSite: "https://nimsum.github.io/movie-tracker/",
+		imgURL: "movie-tracker.jpg",
+		id: "7"
+	},
+	{
+		title: "Star Wars api box",
+		description:
+			"A page that uses the swapi api to generate cards about Star Wars characters/vehicles/planets",
+		team: "Solo Project",
+		toolsUsed: ["React", "SASS", "Jest & Enzyme", "PropTypes"],
+		concepts: ["Async JS", "Organized Workflow", "TDD", "Async JS Testing"],
+		src: "https://github.com/NimSum/swapi-box",
+		staticSite: "https://nimsum.github.io/swapi-box/",
+		imgURL: "swapi-box.jpg",
+		id: "6"
+	},
+	{
+		title: "Marvelously",
+		description:
+			"A page that showcases comics and movies, it has a powerful search and sorting functionality",
+		team: [
+			{ "Hannah Hyde": "https://github.com/alizarincrimson" },
+			{ "Max Silver": "https://github.com/MaxBSilver" },
+			{ "Raechel Odom": "https://github.com/raechelo" }
+		],
+		toolsUsed: ["React", "SASS", "Jest & Enzyme"],
+		concepts: ["Team Work", "TDD", "Git Workflow", "API Fetches"],
+		src: "https://github.com/NimSum/marvel-whateverly",
+		staticSite: "https://nimsum.github.io/marvel-whateverly/",
+		imgURL: "marvelously.jpg",
+		id: "5"
+	},
+	{
+		title: "Wheel of Mortys",
+		description: "Wheel of fortune Rick and Morty Style!",
+		team: [{ "Raechel Odom": "https://github.com/raechelo" }],
+		toolsUsed: ["Webpack", "Mocha & Chai", "SASS"],
+		concepts: ["OOP", "TDD", "Paired Programming", "Git Workflow"],
+		src: "https://github.com/NimSum/wheel-of-mortys",
+		staticSite: "https://nimsum.github.io/wheel-of-mortys/",
+		imgURL: "wheel-of-mortys.jpg",
+		id: "4"
+	},
+	{
+		title: "Connect Four",
+		description: "A replication of the game connect four!",
+		team: "Solo Project",
+		toolsUsed: ["HTML", "CSS", "JS"],
+		concepts: ["Vanilla JS", "Array Prototypes", "Game Structure Design"],
+		src: "https://github.com/NimSum/connect-four-game",
+		staticSite: "https://nimsum.github.io/connect-four-game/index.html",
+		imgURL: "connect-four.jpg",
+		id: "3"
+	},
+	{
+		title: "Static Page Recreation",
+		description:
+			"Re-create a complex page layout, including my own styling and animations",
+		team: "Solo Project",
+		toolsUsed: ["HTML", "CSS", "Chart.js"],
+		concepts: ["BEM/SMACSS", "Grid Areas", "Animations"],
+		src: "https://github.com/NimSum/ng-comp-challenge-2",
+		staticSite: "https://nimsum.github.io/ng-comp-challenge-2/",
+		imgURL: "static-comp.jpg",
+		id: "2"
+	},
+	{
+		title: "Foto Finder",
+		description:
+			"An app that saves images and text to local storage, with features such as: filtering, searching & favorites ",
+		team: "Solo Project",
+		toolsUsed: ["CSS", "HTML", "JS"],
+		concepts: ["es6", "Grid", "Local Storage"],
+		src: "https://github.com/NimSum/foto-finder",
+		staticSite: "https://nimsum.github.io/foto-finder/",
+		imgURL: "foto-finder.jpg",
+		id: "1"
+	}
+];
 
 export const softSkills = [
-  { 
-    title: 'Accessible & Responsive Applications',
-    body: 'Emphasizes the implementation of accessibility and cross-browser/device compatability',
-    id: 6
-  },
-  { 
-    title: 'Problem Solver',
-    body: 'Creative problem solver, enjoys undertaking challenging problems and learning new concepts',
-    id: 5
-  },
-  { 
-    title: 'Git & Github Workflow',
-    body: 'Proficient at using git & github tools to organize and maintain project workflow',
-    id: 4
-  },
-  { 
-    title: 'Communication',
-    body: 'Excelent communicator, values empathetic work environments',
-    id: 3
-  },
-  { 
-    title: 'Test Driven Development',
-    body: 'Skilled at using testing frameworks & libraries such as Jest & Enzyme / Mocha & Chai',
-    id: 2
-  },
-  { 
-    title: 'Object Oriented Programming',
-    body: 'Understands the benefits and concepts of OOP',
-    id: 1
-  }
- ];
+	{
+		title: "Accessible & Responsive Applications",
+		body:
+			"Emphasizes the implementation of accessibility and cross-browser/device compatability",
+		id: 6
+	},
+	{
+		title: "Problem Solver",
+		body:
+			"Creative problem solver, enjoys undertaking challenging problems and learning new concepts",
+		id: 5
+	},
+	{
+		title: "Git & Github Workflow",
+		body:
+			"Proficient at using git & github tools to organize and maintain project workflow",
+		id: 4
+	},
+	{
+		title: "Communication",
+		body: "Excelent communicator, values empathetic work environments",
+		id: 3
+	},
+	{
+		title: "Test Driven Development",
+		body:
+			"Skilled at using testing frameworks & libraries such as Jest & Enzyme / Mocha & Chai",
+		id: 2
+	},
+	{
+		title: "Object Oriented Programming",
+		body: "Understands the benefits and concepts of OOP",
+		id: 1
+	}
+];
 
- export const languagesAndFrameworks = [
-  { 
-    name: 'React',
-    imgName: 'react.svg',
-    id: 8
-  },
-  { 
-    name: 'Redux',
-    imgName: 'redux.svg',
-    id: 7
-  },
-  { 
-    name: 'SASS',
-    imgName: 'sass.svg',
-    id: 6
-  },
-  { 
-    name: 'CSS',
-    imgName: 'css.svg',
-    id: 5
-  },
-  { 
-    name: 'HTML5',
-    imgName: 'html.svg',
-    id: 4
-  },
-  { 
-    name: 'Javascript',
-    imgName: 'js.svg',
-    id: 3
-  },
-  { 
-    name: 'Node.js',
-    imgName: 'node.svg',
-    id: 2
-  },
-  {
-    name: 'Java',
-    imgName: 'java-icon.svg',
-    id: 1
-  }
+export const languagesAndFrameworks = [
+	{
+		name: "React",
+		imgName: "react.svg",
+		id: 8
+	},
+	{
+		name: "Redux",
+		imgName: "redux.svg",
+		id: 7
+	},
+	{
+		name: "SASS",
+		imgName: "sass.svg",
+		id: 6
+	},
+	{
+		name: "CSS",
+		imgName: "css.svg",
+		id: 5
+	},
+	{
+		name: "HTML5",
+		imgName: "html.svg",
+		id: 4
+	},
+	{
+		name: "Javascript",
+		imgName: "js.svg",
+		id: 3
+	},
+	{
+		name: "Node.js",
+		imgName: "node.svg",
+		id: 2
+	}
 ];
