200e0f6916c292c9eeecb725c8fd2181dca42c80
Nimrod Garcia <nimm.garcia@gmail.com>
2019-11-22 15:50:57 -0600

    Prettify code

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -1,38 +1,35 @@
-import React from 'react';
-import { languagesAndFrameworks, softSkills } from '../../utils/PortfolioData';
+import React from "react";
+import { languagesAndFrameworks, softSkills } from "../../utils/PortfolioData";
 
 export const Skills = () => {
+	const generateSoftSkills = () => (
+		<section className="soft-skills-box">
+			{softSkills.map(skill => (
+				<article key={skill.id}>
+					<h3>{skill.title}</h3>
+					<p>{skill.body}</p>
+				</article>
+			))}
+		</section>
+	);
 
-  const generateSoftSkills = () => (
-    <section className="soft-skills-box">
-      { softSkills.map(skill => (
-        <article key={ skill.id }>
-          <h3>{ skill.title }</h3>
-          <p>{ skill.body }</p>
-        </article>
-      ))}
-    </section>
-  )
-  
-  const generateSkills = () => languagesAndFrameworks.map(skill => (
-    <span className="lang-img-cont" key={ skill.id }>
-      { skill.name }
-      <img 
-        src={require(`../../images/${skill.imgName}`)} 
-        alt={ skill.name } 
-        title={ skill.name }/>
-    </span>)
-  )
+	const generateSkills = () =>
+		languagesAndFrameworks.map(skill => (
+			<span className="lang-img-cont" key={skill.id}>
+				{skill.name}
+				<img
+					src={require(`../../images/${skill.imgName}`)}
+					alt={skill.name}
+					title={skill.name}
+				/>
+			</span>
+		));
 
-  return (
-    <section className="skillset-container">
-      <h3 className="skill-box-title">My Skills</h3>
-      <div className="skills-container">
-        { generateSkills() }   
-      </div>
-      <div className="soft-skills">
-        { generateSoftSkills() }
-      </div>
-    </section>
-  )
-}
+	return (
+		<section className="skillset-container">
+			<h3 className="skill-box-title">My Skills</h3>
+			<div className="skills-container">{generateSkills()}</div>
+			<div className="soft-skills">{generateSoftSkills()}</div>
+		</section>
+	);
+};
